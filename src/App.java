742d20552f39ca93f2a88c6ee28fd8eb30ddb3b1
Boby Darmawan <2011522023@student.unand.ac.id>
2022-05-17 09:13:23 +0700

    Add selection sort string

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.io.Console;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -7,30 +9,36 @@ public class App {
         Scanner input = new Scanner(System.in);
         
         int[] nilai = new int[10];
+        String[] kata = new String[10];
+
         int i, imin,  j, k, m, n, temp;
         boolean isContinue = true;
-        int jumlah;
-        String pilihanUser;
+        int jumlah = 0;
+        String pilihan = null, pilihanUser;
 
         System.out.println("==========================");
         System.out.println("  PROGRAM SELECTION SORT  ");
         System.out.println("==========================");
-
+        
         while(isContinue){
             System.out.println("\nJenis urutan:");
             System.out.println("1.Ascending");
             System.out.println("2.Descending");
-            System.out.print("Masukkan pilihan : ");
-            String pilihan = input.next();
-
+            System.out.println("3.String");
+            System.out.print("\nMasukkan pilihan : ");
+            pilihan = input.next();
+        
             switch (pilihan) {
             case "1":
                 System.out.println("\n============================");
                 System.out.println("  SELECTION SORT ASCENDING  ");
                 System.out.println("============================");
                 System.out.print("Masukkan jumlah elemen : ");
-                jumlah = input.nextInt();
-                System.out.println();
+                try {
+                    jumlah = input.nextInt();
+                } catch (InputMismatchException e) {
+                    System.err.println(e);
+                }
         
                 for(m = 0; m < jumlah; m++){
                     System.out.print("Masukkan elemen ke-" + (m+1) + ": ");
@@ -78,7 +86,11 @@ public class App {
                 System.out.println("  SELECTION SORT DESCENDING ");
                 System.out.println("============================");
                 System.out.print("Masukkan jumlah elemen : ");
-                jumlah = input.nextInt();
+                try {
+                    jumlah = input.nextInt();
+                } catch (InputMismatchException e) {
+                    System.err.println(e);
+                }
                 System.out.println();
         
                 for(m = 0; m < jumlah; m++){
@@ -123,6 +135,58 @@ public class App {
                 }
                 break;
             case "3":
+                System.out.println("\n============================");
+                System.out.println("  SELECTION SORT STRING  ");
+                System.out.println("============================");
+                System.out.print("Masukkan jumlah elemen : ");
+                try {
+                    jumlah = input.nextInt();
+                } catch (InputMismatchException e) {
+                    System.err.println(e);
+                }
+                System.out.println();
+        
+                for(m = 0; m < jumlah; m++){
+                    System.out.print("Masukkan elemen ke-" + (m+1) + ": ");
+                    kata[m] = input.next();
+                }
+                
+                System.out.println("\n==========================");
+                System.out.println("Data sebelum diurut");
+                System.out.println("==========================");
+                n = 0;
+                while(n < jumlah){
+                    System.out.println(kata[n]);
+                    n++;
+                }
+                System.out.println();
+        
+                for(i = 0; i < n - 1; i++){
+                    imin = i;
+                    for(j = i+1; j < n; j++){
+                        if( kata[j].compareTo(kata[imin]) > 0 ){
+                            imin=j;
+                        }
+                        String temp2 = kata[imin];
+                        kata[imin] = kata[j];
+                        kata[j] = temp2;
+                    }
+        
+                    System.out.println("Langkah ke " + (i+1));
+                    System.out.println("-----------------------");
+                    for (int x=0;x<jumlah;x++){
+                        System.out.println(kata[x]);
+                    }
+                }
+        
+                System.out.println("\n========================");
+                System.out.println("Data setelah diurut");
+                System.out.println("==========================");
+                for(k = 0; k <= i; k++){
+                    System.out.println(kata[k] + " ");
+                }
+                break;
+            case "4":
                 System.out.println("Terima kasih (> <)");
                 isContinue = false;
             default:
